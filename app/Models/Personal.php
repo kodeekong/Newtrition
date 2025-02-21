02b27994e5768f946980277ce86cada65ba0e83d
Kodee <kodeekong@yahoo.com>
2025-02-21 10:18:01 -0500

    Personal profile, model, migration

---
+++ app/Models/Personal.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelName extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',  // foreign key that links to the users table
+        'age',
+        'gender',
+        'weight',
+        'height_ft',
+        'height_inch',
+        'activity_level',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); 
+    }
+
+}
