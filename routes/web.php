3f87bdb559c98c11c157a4f7a545d2aa66b5c49e
Kodee <kodeekong@yahoo.com>
2025-02-21 08:05:59 -0500

    personal user information page

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,19 @@ use App\Http\Controllers\AuthController;
 
 
 Route::get('/', function () {
-    return view('home'); // You can replace 'welcome' with any view name
+    return view('home'); 
 });
 
 // Authentication Routes
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login'); // Show login form
 Route::post('login', [AuthController::class, 'login']); // Handle login
-Route::post('logout', [AuthController::class, 'logout'])->name('logout'); // Handle logout
+Route::post('logout', [AuthController::class, 'logout'])->name('logout'); // handle logout
 
 Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register'); // Show registration form
-Route::post('register', [AuthController::class, 'register']); // Handle registration
+Route::post('register', [AuthController::class, 'register']); // handle registration
+
+Route::get('personal', [AuthController::class, 'showUserHome'])->name('personal'); // Show user home page
+
 
 // Protected routes
 Route::get('home', [AuthController::class, 'home'])->name('home'); // Show registration form
